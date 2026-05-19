e9910b21392fa6f7f39733934b06fff61d281c61
Faris ProjectTeam305 <faresksa44@gmail.com>
2026-05-20 01:41:42 +0300

    Added Car attributes and constructor

--- src/main/java/com/example/business/Car.java
+++ src/main/java/com/example/business/Car.java
@@ -1,4 +1,23 @@
 package com.example.business;
 
 public class Car {
+    String vin;
+    Long licensePlate;
+    String brand;
+    String model;
+    int year;
+    String bodyType;      // Sedan, Hatchback, Pickup, SUV.
+    int seats;
+    double mileage;
+
+    public Car(String vin, Long licensePlate, String brand, String model, int year, String bodyType, int seats, double mileage) {
+        this.vin = vin;
+        this.licensePlate = licensePlate;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.bodyType = bodyType;
+        this.seats = seats;
+        this.mileage = mileage;
+    }
 }
